7163338fccb34fe810c663ab537be06928769bd6
nefta99 <israelneftali@gmail.com>
2021-08-11 18:44:44 -0500

    dec

--- Datos.js
+++ Datos.js
@@ -1,2 +1,3 @@
 console.log("Inicio de aplicacion desde el administrado gluz99");
 console.log("se agrega otro cambio");
+console.log("se agrega otro cambio con la cuenta de nefta99 israelneftali@gmail.com");
