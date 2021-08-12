5edd0ebe7b7567bf23c0b20ec2ef0fb8e2f8376c
neftec99 <neftalitorres@tec.mx>
2021-08-11 19:09:52 -0500

    un commit mas

--- Datos.js
+++ Datos.js
@@ -1,3 +1,4 @@
 console.log("Inicio de aplicacion desde el administrado gluz99");
 console.log("se agrega otro cambio");
 console.log("se agrega otro cambio con la cuenta de nefta99 israelneftali@gmail.com");
+console.log("se agrega otro cambio con la cuenta de neftec99 neftalitorres@tec.mx");
